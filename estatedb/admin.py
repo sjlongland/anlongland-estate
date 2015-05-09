af1bcf6af6f2684beb2d3a05a702719b14f862d3
Stuart Longland <me@vk4msl.yi.org>
2015-05-10 09:26:41 +1000

    estatedb: Initial models and admin interfaces added

--- estatedb/admin.py
+++ estatedb/admin.py
@@ -1,3 +1,85 @@
 from django.contrib import admin
+import mptt.admin
+import models
+import django.core.urlresolvers
+import django.utils.html
 
-# Register your models here.
+# Source: http://stackoverflow.com/questions/9919780/how-do-i-add-a-link-from-the-django-admin-page-of-one-object-to-the-admin-page-o
+def add_link_field(
+        target_model = None, field = '', app='', field_name='link',
+        link_text=unicode):
+    def add_link(cls):
+        reverse_name = target_model or cls.model.__name__.lower()
+        def link(self, instance):
+            app_name = app or instance._meta.app_label
+            reverse_path = "admin:%s_%s_change" % (app_name, reverse_name)
+            link_obj = getattr(instance, field, None) or instance
+            url = django.core.urlresolvers.reverse(
+                    reverse_path, args = (link_obj.id,))
+            return django.utils.html.mark_safe(
+                    "<a href='%s'>%s</a>" % (url, link_text(link_obj)))
+        link.allow_tags = True
+        link.short_description = reverse_name + ' link'
+        setattr(cls, field_name, link)
+        cls.readonly_fields = list(getattr(cls, 'readonly_fields', [])) + \
+            [field_name]
+        return cls
+    return add_link
+
+@add_link_field()
+class ItemInlineAdmin(admin.TabularInline):
+    model   = models.Item
+    fk_name = 'location'
+    fields  = [
+            'code',
+            'article_type',
+            'name',
+            'description',
+            'claimant',
+            'link',
+    ]
+
+class LocationAdmin(mptt.admin.MPTTModelAdmin):
+    inlines = [
+            ItemInlineAdmin
+    ]
+    list_display_links = [
+            'full_name',
+    ]
+    list_display = [
+            'full_name', 'item_count',
+    ]
+    search_fields = [
+            'parent__name',
+            'name',
+    ]
+    fields = [
+            'name',
+            'parent',
+            'code_prefix',
+            'next_code',
+    ]
+admin.site.register(models.Location, LocationAdmin)
+
+class PhotoInlineAdmin(admin.StackedInline):
+    model   = models.Photo
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [
+            PhotoInlineAdmin
+    ]
+    list_display_links = [
+            'name',
+    ]
+    list_display = [
+            'location',
+            'code',
+            'name',
+    ]
+    search_fields = [
+            'location__full_name',
+            'name',
+            'code',
+    ]
+admin.site.register(models.Item, ItemAdmin)
+admin.site.register(models.ArticleType, admin.ModelAdmin)
--- estatedb/models.py
+++ estatedb/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+import mptt.models
+import django.contrib.auth.models
 
-# Create your models here.
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+class Location(mptt.models.MPTTModel):
+    parent = mptt.models.TreeForeignKey(
+            'self', null=True, blank=True, related_name='children')
+    code_prefix     = models.CharField(max_length=3, unique=True)
+    next_code       = models.PositiveIntegerField(default=0)
+    name            = models.CharField(max_length=64)
+    full_name       = models.CharField(max_length=256)
+
+    path            = property(lambda s : \
+            ((s.parent and s.parent.path) or []) + [s.name])
+    parent_name     = property(lambda s : (s.parent and \
+            s.parent.full_name) or None)
+    item_count      = property(lambda s : s.item_set.count() + sum([\
+            c.item_count for c in s.children.all()] + [0]))
+
+    def __unicode__(self):
+        return self.full_name
+
+    def save(self, *args, **kwargs):
+        self.full_name = u'/'.join(self.path)
+        return super(Location, self).save(*args, **kwargs)
+
+    class Meta:
+        unique_together = ('parent','name')
+        ordering        = ['name']
+
+    def _get_new_code(self, next_code=None):
+        return u'%s_%08d' % (self.code_prefix, next_code or self.next_code)
+
+    def get_new_code(self):
+        code = self._get_new_code()
+        next_code = self.next_code + 1
+        while self.child_items.filter(\
+                code__exact=self._get_new_code(next_code)).count() > 0:
+            logging.info('%d in use', next_code)
+            next_code += 1
+        self.next_code = next_code
+        self.save()
+        return code
+
+class ArticleType(models.Model):
+    name                = models.CharField(max_length=64, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering        = ['name']
+
+class Item(models.Model):
+    code                = models.CharField(max_length=12,
+                            unique=True, null=True, blank=True)
+    location            = models.ForeignKey(Location,
+                            related_name='child_items')
+    article_type        = models.ForeignKey(ArticleType)
+    name                = models.CharField(max_length=64)
+    description         = models.CharField(max_length=256)
+    claimant            = models.ForeignKey(
+            django.contrib.auth.models.User, null=True, blank=True)
+    contents            = models.ForeignKey(Location, null=True,
+            blank=True, related_name='parent_item')
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not bool(self.code):
+            logging.info('Generating new code')
+            self.code = self.location.get_new_code()
+            logging.info('New code: %s', self.code)
+        super(Item, self).save(*args, **kwargs)
+
+class Photo(models.Model):
+    item                = models.ForeignKey(Item)
+    photo               = models.ImageField()

f3dde4328a8f2edea2e5bfcd842edf4ca0980d5b
Stuart Longland <me@vk4msl.yi.org>
2015-05-10 14:40:29 +1000

    estatedb: App naming

---
+++ estatedb/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'estatedb.apps.EstateConfig'
---
+++ estatedb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class EstateConfig(AppConfig):
+    name = 'estatedb'
+    verbose_name = "Estate"
+    label = 'estatedb'

062fa37d05714f9a30c66b08a0377bb064ec56bc
Stuart Longland <me@vk4msl.yi.org>
2015-05-10 14:39:48 +1000

    estate.urls: Add URL maps for static pages

--- estate/urls.py
+++ estate/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import estatedb.views
+#from django.conf import settings
+#from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'estate.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^/*$', estatedb.views.index, name='index'),
+    url(r'^location/(?P<location_id>[0-9]+)/$',
+        estatedb.views.location, name='location'),
+    url(r'^item/(?P<item_id>[0-9]+)/$',
+        estatedb.views.item, name='item'),
+    url(r'^photo/(?P<photo_id>[0-9]+)/$',
+        estatedb.views.photo, name='photo'),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+)# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
